74a427cf9c7fd736010ee033bc2df7f8aad00194
adoherty8 <azdoherty91@gmail.com>
2019-07-20 09:31:19 -0400

    solution tester added:

---
+++ solution_tester.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class SolutionTest:
+	def __init__(self, solution_function_name, test_cases):
+		self.solution_function_name = solution_function_name
+		self.test_cases = test_cases
+		self.times
+
+	def funcname(self, parameter_list):
+		pass
